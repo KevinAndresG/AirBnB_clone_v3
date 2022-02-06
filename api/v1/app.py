1450d810229d33255c4f222e73e4e197e6968114
knia10 <kelly.stephania@hotmail.com>
2022-02-06 17:47:49 -0500

    Modified style

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,10 @@ from api.v1.views import app_views
 from os import getenv
 
 
-
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def storage_close(exception):
@@ -23,6 +23,6 @@ def storage_close(exception):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", default="0.0.0.0" ),
+    app.run(host=getenv("HBNB_API_HOST", default="0.0.0.0"),
             port=int(getenv("HBNB_API_PORT", default=5000)),
             threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,12 +7,14 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status')
 def json_status():
     '''
     returns a JSON: "status": "OK"
     '''
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats')
 def num_obj():
